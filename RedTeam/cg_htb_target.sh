368c38351835cc941f4d8013d354637f2d79a203
root <git@tdcme.com>
2023-08-24 10:27:28 -0700

    HTB script

---
+++ RedTeam/cg_htb_target.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+#
+# Author: Bret.s
+# Last Update: 8/24/2023
+# Version: 0.1
+# Made In: Kali 2 Rolling
+# OS Tested: Kali 2 Rolling
+# Purpose:
+#
+# Command Line Usage: ./"
+##
+# Note:
+##
+# Version Change Notes.
+# 0.1 -
+##
+
+targetName="${1}"
+targetIP="${2}"
+
+targetIPName="$(echo "${targetIP}"| tr '/' '-')"
+outFileQuickScan="${targetIPName}_scan-log"
+outFileFullScan="${outFileQuickScan}_full"
+
+
+# Settings
+nmapSpeed='T4'
+
+cmdHelp(){
+    echo "Usage: ./cg_htb_target.sh <target name> <target IP>"
+    echo "Example: ./cg_htb_target.sh 'target.htb' '10.10.11.233'"
+}
+
+if [ "${targetName}" == '' ]; then echo "missing target name..";cmdHelp;exit 1;fi
+if [ "${targetIP}" == '' ]; then echo "missing target IP..";cmdHelp;exit 1;fi
+
+# Create target directory.
+mkdir -p "./${targetName}"
+cd "./${targetName}" || exit 1
+
+# Set DNS to resolve to target IP.
+echo "address=/.${targetName}/${targetIP}" >>/etc/dnsmasq.conf
+systemctl stop dnsmasq.service && systemctl start dnsmasq.service
+if [ -z "$(dig +short "sdfg.${targetName}")" ];then echo 'DNS Config did not work, check resolver.conf';exit 1;else echo 'DNS Good' ;fi
+
+
+fun_quickPortScan(){
+    #
+    nmap -Pn -${nmapSpeed} --top-ports 1000 -oA "./${outFileQuickScan}" ${targetIP}
+}
+
+fun_scanAllPorts(){
+    #
+    nmap -Pn -${nmapSpeed} -p- --open -oA "./${outFileFullScan}" ${targetIP}
+}
+
+fun_scanPortsDetails(){
+    tmp_ipList="${outFileFullScan}.gnmap"
+    declare -a ary_linesOfFile ary_tmp_openPort
+    readarray -t ary_linesOfFile <<<"""$(grep 'Ports:' "./${tmp_ipList}" |grep -i open)"""
+    for str_tmp_line in "${ary_linesOfFile[@]}";do
+        # Put Host IP in to array.
+        str_hostIP="$(echo "${str_tmp_line}"|awk -F'\t' '{print $1}'|tr -d '(\|)\| '|cut -c 6-)"
+        # Parse data to form NMap ready variable of open ports.
+        readarray -d ',' ary_tmp_openPort <<<"""$(echo "${str_tmp_line}"|awk -F'\t' '{print $2}'|tr -d '(\|)\| '|cut -c 7-)"""
+        str_nmapPortList=''
+        for str_port in "${ary_tmp_openPort[@]}";do
+            str_tmp_ports="$(echo "${str_port}"|awk -F'/' '{print $1}')"
+            str_nmapPortList+="${str_tmp_ports},"
+        done
+        str_openPort="${str_nmapPortList:0:-1}"
+        #
+        nmap -Pn -p "${str_openPort}" -oA "./${str_hostIP}_detail-port-scan" -sCV -A -T3 "${str_hostIP}"
+    done
+
+}
+
+
+fun_quickPortScan
+fun_scanAllPorts
+fun_scanPortsDetails
+exit 0
